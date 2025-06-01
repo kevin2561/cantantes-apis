b3cc2d5280d0702719545986bd85b458dc39f552
KEVIN NOA ROMERO <kevinnoa02@gmail.com>
2025-05-31 21:25:51 -0500

    onetomany java

--- src/main/java/com/apirestsql/apirest/Entites/Genero.java
+++ src/main/java/com/apirestsql/apirest/Entites/Genero.java
@@ -1,10 +1,13 @@
 package com.apirestsql.apirest.Entites;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,6 +23,9 @@ public class Genero {
 
     @Column(name = "descripcion")
     private String descripcion;
+
+    @OneToMany(mappedBy = "genero")
+    private List<Cantante> cantantes;
 
     public Long getIdGenero() {
         return idGenero;
--- src/main/java/com/apirestsql/apirest/Repository/GeneroRepository.java
+++ src/main/java/com/apirestsql/apirest/Repository/GeneroRepository.java
@@ -2,11 +2,27 @@ package com.apirestsql.apirest.Repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.apirestsql.apirest.Entites.Genero;
 
 @Repository
 public interface GeneroRepository extends JpaRepository<Genero, Long> {
+//     @Query("select * from usuarios")
+
+//     SELECT 
+//     g.idGenero as idGenero, 
+// 	g.nombre AS nombreGenero,
+// 	g.descripcion as descripcion ,
+//     COUNT(c.idCantante) AS cantidadCantantes
+// FROM 
+//     genero g
+// LEFT JOIN 
+//     cantantes c ON g.idGenero = c.idGenero
+// GROUP BY 
+//     g.idGenero, g.nombre, g.descripcion
+// ORDER BY 
+//     g.idGenero;
 
 }
